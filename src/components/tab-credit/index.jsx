ec470552bca98120bc28fe24f1e0f15ae7682ec2
nv <naverstay.me@gmail.com>
2020-10-27 10:30:27 +0200

    new osago popup

--- src/components/tab-credit/index.jsx
+++ src/components/tab-credit/index.jsx
@@ -263,7 +263,8 @@ class TabCredit extends Component {
     }
 
     componentDidMount() {
-        this.props.allFields && this.setState({showAdditional: true, newCar: false})
+        this.props.allFields && this.setState({showAdditional: true, newCar: false});
+        this.scrollToBottom();
     }
 
     componentDidUpdate() {
@@ -274,7 +275,7 @@ class TabCredit extends Component {
                 mask[key] = m.split(':')[1]
             })
             Inputmask(mask).mask(inp);
-        })
+        });
 
         document.querySelectorAll('[data-inputmask-date]').forEach(function (inp) {
             Inputmask({
@@ -282,7 +283,8 @@ class TabCredit extends Component {
                 showMaskOnHover: false,
                 regex: String.raw`^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$`
             }).mask(inp);
-        })
+        });
+        this.scrollToBottom();
     };
 
     updateImage = (img) => {
